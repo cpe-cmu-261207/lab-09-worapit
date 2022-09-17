fda38d4d1454560f01687efbe4dfd1134a2a06aa
worapit <drm.wmwork@gmail.com>
2022-09-17 22:22:15 +0700

    Update 2

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 export default function contact() {
   return (
-    <div>
+    <div style={{ backgroundColor: "indianred" }}>
       <div>
         <Navbar
           home="btn btn-outline-light"
